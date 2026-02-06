785d1c641f93779103f104a41410d38ed3f4b200
kai <1376678+hulloitskai@users.noreply.github.com>
2026-02-06 03:13:34 -0500

    Add typings to Authentication module

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,15 +1,30 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 module Authentication
+  extend T::Sig
+  extend T::Helpers
+
+  requires_ancestor { ApplicationController }
+
   extend ActiveSupport::Concern
 
   included do
+    extend T::Sig
+
+    T.bind(self, T.class_of(ApplicationController))
+
     before_action :require_authentication
     helper_method :authenticated?
   end
 
   class_methods do
+    extend T::Sig
+    extend T::Helpers
+
+    requires_ancestor { T.class_of(ApplicationController) }
+
+    sig { params(options: T.untyped).void }
     def allow_unauthenticated_access(**options)
       skip_before_action(:require_authentication, **options)
     end
@@ -17,31 +32,38 @@ module Authentication
 
   private
 
+  sig { returns(T::Boolean) }
   def authenticated?
-    resume_session
+    !!resume_session
   end
 
+  sig { void }
   def require_authentication
     resume_session || request_authentication
   end
 
+  sig { returns(T.nilable(Session)) }
   def resume_session
     Current.session ||= find_session_by_cookie
   end
 
+  sig { returns(T.nilable(Session)) }
   def find_session_by_cookie
     Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
   end
 
+  sig { void }
   def request_authentication
     session[:return_to_after_authenticating] = request.url
     redirect_to(new_session_path)
   end
 
+  sig { returns(String) }
   def after_authentication_url
     session.delete(:return_to_after_authenticating) || root_url
   end
 
+  sig { params(user: User).returns(Session) }
   def start_new_session_for(user)
     user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
       Current.session = session
@@ -49,6 +71,7 @@ module Authentication
     end
   end
 
+  sig { returns(T.nilable(String)) }
   def terminate_session
     Current.session.destroy
     cookies.delete(:session_id)
