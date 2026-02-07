ceee00cdce7f98902b610c51031a5b3d26d28bcf
kai <1376678+hulloitskai@users.noreply.github.com>
2026-02-07 10:23:25 -0500

    Fix Fly deployment (bind on 0.0.0.0)

--- config/initializers/canonical_host.rb
+++ config/initializers/canonical_host.rb
@@ -4,5 +4,14 @@
 return unless Rails.env.production?
 
 if (host = ENV["CANONICAL_HOST"])
-  Rails.application.config.middleware.use(Rack::CanonicalHost, host)
+  Rails.application.config.middleware.use(
+    Rack::CanonicalHost,
+    host,
+    cache_control: "no-cache",
+    if: ->(uri) {
+      uri.host != "localhost" &&
+        uri.host != "127.0.0.1" &&
+        uri.host != "0.0.0.0"
+    },
+  )
 end
