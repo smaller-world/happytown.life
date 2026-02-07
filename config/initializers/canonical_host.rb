805852ba31f7f5f0da92c929dc7a491ab67e41f4
kai <1376678+hulloitskai@users.noreply.github.com>
2026-02-07 16:47:17 -0500

    Configure Fly to work with basecamp/thruster

--- config/initializers/canonical_host.rb
+++ config/initializers/canonical_host.rb
@@ -8,11 +8,7 @@ Rails.application.config.middleware.use(
   canonical_host,
   cache_control: "no-cache",
   if: ->(uri) {
-    # Ignore local requests
-    return false if uri.host.nil?
-    return false if uri.host == "0.0.0.0" || uri.host == "::"
-
-    ip_addr = IPAddr.new(uri.host)
-    !ip_addr.loopback?
+    # Ignore healthcheck requests
+    uri.path != "/up"
   },
 )
