a1623acf86133a09c4a63b12e56f90ab07cf7f0f
kai <1376678+hulloitskai@users.noreply.github.com>
2026-02-10 17:21:06 -0500

    Attach Sentry to Rails.error reporter

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -17,6 +17,9 @@ Sentry.init do |config|
     "ActiveRecord::RecordNotUnique",
   ]
 
+  # Automatically attach to Rails error reporter
+  config.rails.register_error_subscriber = true
+
   # Add data like request headers and IP for users,
   # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
   config.send_default_pii = true
