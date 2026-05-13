dbdee240c9cb4e5f2c2ec5ed75be0f327a1ca716
kai <1376678+hulloitskai@users.noreply.github.com>
2026-05-13 07:34:05 -0400

    Temp solution to remove scam messages

--- app/controllers/wsapi_controller.rb
+++ app/controllers/wsapi_controller.rb
@@ -49,9 +49,18 @@ class WsapiController < ApplicationController
     if (community_id = wsapi.community_id(group_id: message.chat_id))
       is_admin = wsapi.community_admin?(community_id:, participant: message.sender.id)
       if is_admin
-        wsapi.send_message(to: message.chat_id, text: "👀")
+        # wsapi.send_message(to: message.chat_id, text: "👀")
+        wsapi.delete_message(
+          message_id: message.id,
+          chat_id: message.chat_id,
+          sender_id: message.sender.id,
+        )
+        wsapi.remove_community_participants(
+          community_id:,
+          participants: [ message.sender.id ],
+        )
       else
-        wsapi.send_message(to: message.chat_id, text: "nope")
+        # wsapi.send_message(to: message.chat_id, text: "nope")
         wsapi.delete_message(
           message_id: message.id,
           chat_id: message.chat_id,
